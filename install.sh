61d2723fdbcfaab5d5fcd7ee75e4d9441b152af9
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-05-14 00:36:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ apt install -y clang clangd clang-tidy clang-format lld llvm cmake ninja-build
 wget -O mspkg.deb https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 dpkg -i mspkg.deb
 rm mspkg.deb
-apt-add-repository https://packages.microsoft.com/ubuntu/22.04/prod
+apt-add-repository -y https://packages.microsoft.com/ubuntu/22.04/prod
 apt update
 apt install -y dotnet-sdk-6.0
 
