59db6a762ba1bbe28abeb6693dc6db017202eb19
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-05-14 00:38:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-## Core system
+# Core system
 
-# base desktop
+## base desktop
 apt install -y gnome-shell gnome-terminal gnome-tweaks gnome-system-monitor gnome-disk-utility gnome-shell-extensions nautilus ubuntu-drivers-common cups linux-sound-base alsa-base alsa-utils flatpak apt-transport-https software-properties-common curl
 apt purge -y gedit
 apt autoremove -y --purge
 
-# install core drivers
+## install core drivers
 ubuntu-drivers install
 
-# install hp printer driver
+## install hp printer driver
 apt install -y hplip
 
-# flatpak and flathub
+## flatpak and flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub org.gnome.eog
 flatpak install -y flathub org.gnome.Epiphany
@@ -24,10 +24,10 @@ flatpak install -y flathub com.mattjakeman.ExtensionManager
 flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.wps.Office
 
-# clang
+## clang
 apt install -y clang clangd clang-tidy clang-format lld llvm cmake ninja-build
 
-# dotnet 6.0
+## dotnet 6.0
 wget -O mspkg.deb https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 dpkg -i mspkg.deb
 rm mspkg.deb
@@ -35,41 +35,41 @@ apt-add-repository -y https://packages.microsoft.com/ubuntu/22.04/prod
 apt update
 apt install -y dotnet-sdk-6.0
 
-# atom ide
+## atom ide
 wget -O atom.deb https://atom.io/download/deb
 apt install -y xdg-utils
 dpkg -i atom.deb
 rm atom.deb
 apm install platformio-ide-terminal
 
-# steam games
+## steam games
 wget -O steam.deb https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 dpkg -i steam.deb
 rm steam.deb
 
-## Customization
+# Customization
 
-# fonts
+## fonts
 apt install -y fonts-inter
 
-# cursors
+## cursors
 apt install -y yaru-theme-icon
 
-# gtk theme (fluent)
+## gtk theme (fluent)
 git clone https://github.com/vinceliuice/fluent-gtk-theme
 cd fluent-gtk-theme
 ./install.sh --color dark --tweaks round --tweaks noborder --tweaks square
 cd ..
 rm -rf fluent-gtk-theme
 
-# icon theme (fluent)
+## icon theme (fluent)
 git clone https://github.com/vinceliuice/fluent-icon-theme
 cd fluent-icon-theme
 ./install.sh --round
 cd ..
 rm -rf fluent-icon-theme
 
-# windows 10 sounds
+## windows 10 sounds
 cd linux-setup
 mkdir -p .local/share/sounds
 tar -xf msaudio.tar.xz -C .local/share/sounds
