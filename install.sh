822954942485f708809019be733bcdc3f416e695
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-06-12 20:15:30 +0200

    Gedit sucked more than I hoped

--- install.sh
+++ install.sh
@@ -4,15 +4,14 @@
 
 ## base desktop
 apt install -y gnome-shell gnome-terminal gnome-tweaks gnome-system-monitor gnome-disk-utility gnome-shell-extensions nautilus ubuntu-drivers-common cups linux-sound-base alsa-base alsa-utils flatpak apt-transport-https software-properties-common curl
+apt purge gedit
+apt autoremove --purge
 
 ## core drivers
 ubuntu-drivers install
 
 ## hp printer driver
 apt install -y hplip
-
-## gedit
-apt install -y gedit gedit-plugins
 
 ## flatpak and flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -25,6 +24,13 @@ flatpak install -y flatpak com.github.maoschanz.drawing
 flatpak install -y flathub com.mattjakeman.ExtensionManager
 flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.wps.Office
+
+## atom ide
+wget -O atom.deb https://atom.io/download/deb
+apt install -y xdg-utils
+dpkg -i atom.deb
+rm atom.deb
+apm install platformio-ide-terminal git-log file-icons nord-atom-ui
 
 ## clang
 apt install -y clang clangd clang-tidy clang-format lld llvm cmake ninja-build
