788efc6645bcb6eb5255d56b59824871c1ce5dac
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-07-06 01:16:59 +0200

    Update and rename install-miniiso.sh to install-apps.sh

---
+++ install-apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt install gnome-software gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub org.gnome.eog
+flatpak install -y flathub org.gnome.Epiphany
+flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.wps.Office
+flatpak install -y flathub com.valvesoftware.Steam
+flatpak install -y flathub io.atom.Atom
