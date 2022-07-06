0449e6d34fe8592c05a36b05d61b096848f5bdb3
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-07-06 11:36:27 +0200

    Update install-flatpaks.sh

--- install-flatpaks.sh
+++ install-flatpaks.sh
@@ -4,6 +4,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub org.gnome.eog
 flatpak install -y flathub org.gnome.Epiphany
+flatpak install -y flathub org.gnome.Builder
 flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub com.mattjakeman.ExtensionManager
 flatpak install -y flathub com.discordapp.Discord
