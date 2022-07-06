df5181e4830cbf94c39f2164614e2bc1bf142a65
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-07-06 15:18:10 +0200

    Update debloat-ubuntu.sh

--- debloat-ubuntu.sh
+++ debloat-ubuntu.sh
@@ -20,9 +20,18 @@ sudo apt purge ubuntu-desktop         # full ubuntu desktop metapackage
 sudo apt purge ubuntu-desktop-minimal # minimal ubuntu desktop metapackage
 
 # reinstall important stuff
-sudo apt install --no-install-recommends linux-sound-base acpi-support gnome-shell gnome-shell-extension-prefs gnome-terminal gnome-system-monitor gnome-bluetooth gnome-session-canberra gnome-software nautilus gsettings-ubuntu-schemas ubuntu-settings
+sudo apt install --no-install-recommends linux-sound-base            # audio driver
+sudo apt install --no-install-recommends acpi-support                # acpi event support for certain devices
+sudo apt install --no-install-recommends gnome-session gdm3          # gnome 40 core
+sudo apt install --no-install-recommends gnome-shell-extension-prefs # gnome extensions system
+sudo apt install --no-install-recommends gnome-terminal              # terminal
+sudo apt install --no-install-recommends gnome-system-monitor        # system monitor
+sudo apt install --no-install-recommends gnome-bluetooth             # bluetooth
+sudo apt install --no-install-recommends gnome-software              # software
+sudo apt install --no-install-recommends nautilus                    # files
+sudo apt install --no-install-recommends ubuntu-settings             # settings
 
-# reinstall theming
+# reinstall yaru theme
 sudo apt install --no-install-recommends yaru-theme-gnome-shell yaru-theme-gtk yaru-theme-sound yaru-theme-icon ubuntu-wallpapers 
 
 # remove remains
