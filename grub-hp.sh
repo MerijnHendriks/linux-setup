5ba67704a628ca3445cbbe74baa8b7a838e49960
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-07-06 10:32:00 +0200

    Update grub-hp.sh

--- grub-hp.sh
+++ grub-hp.sh
@@ -3,7 +3,6 @@ git clone https://github.com/AdisonCavani/distro-grub-themes.git
 sudo mkdir /boot/grub/themes
 cd distro-grub-themes/customize
 sudo cp -r hp/ /boot/grub/themes
-# sudo nano /etc/default/grub > change GFX_MODE to match resolution
 sudo update-grub
 cd ../../
 rm -rf distro-grub-themes
