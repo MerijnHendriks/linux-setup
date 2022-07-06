480222b2fe7fe3e1ca942642d22e491dca896a71
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-07-06 12:25:21 +0200

    Update install-flatpaks.sh

--- install-flatpaks.sh
+++ install-flatpaks.sh
@@ -2,11 +2,12 @@
 sudo apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.github.tchx84.Flatseal
-flatpak install -y flathub org.gnome.eog
-flatpak install -y flathub org.gnome.Epiphany
-flatpak install -y flathub org.gnome.Builder
-flatpak install -y flathub io.github.celluloid_player.Celluloid
 flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub org.gnome.Epiphany
+flatpak install -y flathub org.gnome.TextEditor
+flatpak install -y flathub org.gnome.eog
+flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub org.gnome.Builder
 flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.wps.Office
 flatpak install -y flathub com.valvesoftware.Steam
