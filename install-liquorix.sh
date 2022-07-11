b1fa1ea32659a0117d817fac221d5b537673d173
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-07-11 22:39:36 +0200

    Update install-liquorix.sh

--- install-liquorix.sh
+++ install-liquorix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo add-apt-repository ppa:damentz/liquorix
+sudo add-apt-repository -y ppa:damentz/liquorix
 sudo apt-get update
-sudo apt-get install --no-install-recommends linux-image-liquorix-amd64 linux-headers-liquorix-amd64
+sudo apt-get install -y --no-install-recommends linux-image-liquorix-amd64 linux-headers-liquorix-amd64
