c3fdb446ba7fc547cdce62e3dcfbd080a46f8bf8
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-07-22 18:51:04 +0200

    Update install-applications.sh

--- install-applications.sh
+++ install-applications.sh
@@ -4,18 +4,27 @@
 apt install --no-install-recommends gnome-tweaks
 
 # flatpak
- apt install --no-install-recommends gnome-software-plugin-flatpak xdg-desktop-portal-gtk xdg-desktop-portal-gnome
- flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
- flatpak install -y flathub com.github.tchx84.Flatseal
- flatpak install -y flathub com.mattjakeman.ExtensionManager
- flatpak install -y flathub org.gnome.FileRoller
- flatpak install -y flathub org.gnome.Epiphany
- flatpak install -y flathub org.gnome.TextEditor
- flatpak install -y flathub org.gnome.eog
- flatpak install -y flathub io.github.celluloid_player.Celluloid
- flatpak install -y flathub org.gnome.Builder
- flatpak install -y flathub com.discordapp.Discord
- flatpak install -y flathub com.wps.Office
- flatpak install -y flathub com.spotify.Client
- flatpak install -y flathub com.valvesoftware.Steam
- flatpak install -y flathub com.github.Matoking.protontricks
+apt install --no-install-recommends gnome-software-plugin-flatpak xdg-desktop-portal-gtk xdg-desktop-portal-gnome
+ 
+# flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update --appstream
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub org.gnome.FileRoller
+flatpak install -y flathub org.gnome.Epiphany
+flatpak install -y flathub org.gnome.TextEditor
+flatpak install -y flathub org.gnome.eog
+flatpak install -y flathub io.github.celluloid_player.Celluloid
+flatpak install -y flathub org.gnome.Builder
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.wps.Office
+flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub com.valvesoftware.Steam
+flatpak install -y flathub com.github.Matoking.protontricks
+
+# lutis
+flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+flatpak update --appstream
+flatpak install -y flathub org.gnome.Platform.Compat.i386 org.freedesktop.Platform.GL32.default org.freedesktop.Platform.GL.default
+flatpak install -y flathub-beta net.lutris.Lutris
