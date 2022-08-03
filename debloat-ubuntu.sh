88cc0752ba5a46b7aa2da438b1215a8bd191685c
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-08-03 15:04:37 +0200

    Update debloat-ubuntu.sh

--- debloat-ubuntu.sh
+++ debloat-ubuntu.sh
@@ -98,7 +98,7 @@ rm /usr/share/applications/software-properties-drivers.desktop                  
 rm /usr/share/applications/gnome-language-selector.desktop                       # language support icon
 
 # update all packages
-apt full-upgrade -y
+apt dist-upgrade -y
 
 # remove remains
 apt autoremove -y --purge
