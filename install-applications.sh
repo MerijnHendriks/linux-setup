69fb5f72233e1d2457833f8361380ffbbde1127e
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-09-09 12:38:13 +0200

    Update install-applications.sh

--- install-applications.sh
+++ install-applications.sh
@@ -2,6 +2,7 @@
 
 # apt
 apt install -y --no-install-recommends gnome-tweaks
+apt purge -y gnome-terminal
 
 # flatpak
 apt install -y --no-install-recommends gnome-software-plugin-flatpak xdg-desktop-portal-gtk xdg-desktop-portal-gnome
@@ -12,6 +13,7 @@ flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/fl
 flatpak update --appstream
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub com.raggesilver.BlackBox
 flatpak install -y flathub org.gnome.FileRoller
 flatpak install -y flathub org.gnome.Epiphany
 flatpak install -y flathub org.gnome.TextEditor
