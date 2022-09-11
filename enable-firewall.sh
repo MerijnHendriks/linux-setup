725a6705563a2db209da6427fe30e15b398a6f41
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-09-11 17:26:42 +0200

    Delete enable-firewall.sh

--- enable-firewall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ufw default deny incoming  # disable all incoming ports
-ufw default allow outgoing # enable all outgoing ports
-ufw allow 80/tcp           # enable http
-ufw allow 443/tcp          # enable https
-ufw enable                 # turn on the firewall
