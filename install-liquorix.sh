7fa499d6afff25901f4131a7f850009c003dd548
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-09-11 17:27:10 +0200

    Delete install-liquorix.sh

--- install-liquorix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo add-apt-repository -y ppa:damentz/liquorix
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends linux-image-liquorix-amd64 linux-headers-liquorix-amd64
