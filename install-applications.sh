7322b71a45a0aa833b6b82cc1729f962ad1ca80d
Merijn Hendriks <54717671+MerijnHendriks@users.noreply.github.com>
2022-09-11 14:13:09 +0200

    Update install-applications.sh

--- install-applications.sh
+++ install-applications.sh
@@ -5,11 +5,11 @@ apt install -y --no-install-recommends gnome-tweaks
 apt purge -y gnome-terminal
 
 # flatpak
-apt install -y --no-install-recommends gnome-software-plugin-flatpak xdg-desktop-portal-gtk xdg-desktop-portal-gnome
+# apt install -y --no-install-recommends gnome-software-plugin-flatpak 
+apt install -y --no-install-recommends flatpak xdg-desktop-portal-gtk xdg-desktop-portal-gnome
  
 # flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 flatpak update --appstream
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub com.mattjakeman.ExtensionManager
@@ -19,8 +19,8 @@ flatpak install -y flathub org.gnome.Epiphany
 flatpak install -y flathub org.gnome.TextEditor
 flatpak install -y flathub org.gnome.eog
 flatpak install -y flathub io.github.celluloid_player.Celluloid
-flatpak install -y flathub com.discordapp.Discord
+# flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.wps.Office
-flatpak install -y flathub com.spotify.Client
+# flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub com.valvesoftware.Steam
 flatpak install -y flathub com.github.Matoking.protontricks
